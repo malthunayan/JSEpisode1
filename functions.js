e3613a2d857d2647cdd979cbd4922de5b2a13fa8
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-09-15 11:00:50 +0300

    Mohammad Al Thunayan

--- functions.js
+++ functions.js
@@ -81,25 +81,14 @@ function squareOrDouble(n) {
 function ageFromBirthDate(birthDate) {
   const _MS_PER_YEAR = 1000 * 60 * 60 * 24 * 365;
   // Your code here
-  var now = new Date();
-
-  function isLeap(year) {
-    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+  var year = +(birthDate.slice(0,4))
+  var month = +(birthDate.slice(4,6))
+  var date = new Date()
+  if (date.getMonth() > month) {
+    return (date.getFullYear() - year)
+  } else {
+    return ((date.getFullYear() - year ) - 1)
   }
-
-  // days since the birthdate    
-  var days = Math.floor((now.getTime() - birthDate.getTime())/1000/60/60/24);
-  var age = 0;
-  // iterate the years
-  for (var y = birthDate.getFullYear(); y <= now.getFullYear(); y++){
-    var daysInYear = isLeap(y) ? 366 : 365;
-    if (days >= daysInYear){
-      days -= daysInYear;
-      age++;
-      // increment the age only if there are available enough days for the year.
-    }
-  }
-  return age;
 }
 
 console.log(Date.now)
